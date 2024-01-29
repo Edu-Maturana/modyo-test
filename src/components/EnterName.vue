d85d8851ccbad38c68feffd190785378a6af03e0
Edu-Maturana <edu.maturana.c@gmail.com>
2024-01-29 18:18:04 -0300

    feat: improve accesibility in enter name screen

--- src/components/EnterName.vue
+++ src/components/EnterName.vue
@@ -3,28 +3,32 @@
     <h1 class="mb-3">Bienvenido a Memory Game</h1>
 
     <div class="mb-3 w-100">
+      <label for="playerName" class="visually-hidden">Ingresa tu nombre</label>
       <input
         v-model="name"
+        id="playerName"
         class="form-control"
         placeholder="Ingresa tu nombre"
         @keyup.enter="saveName"
         autofocus
       />
     </div>
-    <button @click="saveName" class="btn btn-primary w-100">Comenzar a jugar</button>
+
+    <button @click="saveName" class="btn btn-primary w-100" tabindex="0" :disabled="!submitEnabled">
+      Comenzar a jugar
+    </button>
   </div>
 </template>
 
 <script setup>
-import { ref, defineEmits } from 'vue'
+import { ref, defineEmits, computed } from 'vue'
 
 const emit = defineEmits()
 const name = ref('')
+const submitEnabled = computed(() => name.value.trim() !== '')
 
 const saveName = () => {
-  if (name.value.trim() !== '') {
-    localStorage.setItem('playerName', name.value)
-    emit('name-submitted', name.value)
-  }
+  localStorage.setItem('playerName', name.value)
+  emit('name-submitted', name.value)
 }
 </script>
